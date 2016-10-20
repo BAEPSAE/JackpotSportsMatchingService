59c4df496706e331640d8c067f9857cf47bfd8c4
jimin <afrocat@naver.com>
2016-10-20 15:59:00 +0900

    player commit

--- MatchingService/src/action/PlayerAction.java
+++ MatchingService/src/action/PlayerAction.java
@@ -6,6 +6,7 @@ import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import dao.PlayerDAO;
 import vo.Player;
 import vo.Team;
 
@@ -14,6 +15,7 @@ public class PlayerAction extends ActionSupport implements SessionAware {
 	/*Team team;
 	Matching matching;*/
 	Map<String, Object> session;
+	PlayerDAO dao=new PlayerDAO();
 	
 	String loginId;	//session 확인용
 	String message;	//에러 메세지 전송용
@@ -38,12 +40,29 @@ public class PlayerAction extends ActionSupport implements SessionAware {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-
 	
 	//method
+	//개인정보 가져오기
+	public String getUserInfo() {
+		loginId=(String)session.get("loginId");
+		System.out.println(">> "+loginId+"로 정보를 가져옵니다.");
+		return SUCCESS;
+	}
 	
+	//종목별 전적 가져오기
+	public String getUserRecord() {
+		return SUCCESS;
+	}
+	
+	//종목별 경기장 정보 가져오기
+	public String getUserGround() {
+		return SUCCESS;
+	}
+	
+	//session
 	@Override
 	public void setSession(Map<String, Object> arg0) {
 		session=arg0;
 	}
+	
 }
--- MatchingService/src/dao/PlayerDAO.java
+++ MatchingService/src/dao/PlayerDAO.java
@@ -1,5 +1,43 @@
 package dao;
 
-public class PlayerDAO {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+
+import vo.Grounds;
+import vo.Player;
+import vo.Record;
+
+public class PlayerDAO {
+	SqlSession sqlSession=MybatisConfig.getSqlSessionFactory().openSession();
+	
+	//개인정보 가져오기
+	public Player getUserInfo(String user_Id) {
+		Player getInfo=new Player();
+		sqlSession.selectOne("mapper.PlayerMapper.getUserInfo", user_Id);
+		return getInfo;
+	}
+	
+	//종목별 전적 가져오기
+	public Record getUserRecord(String user_Id, String sports) {
+		Map<String, String> map=new HashMap<>();
+		map.put("user_Id", user_Id);
+		map.put("sports", sports);
+		
+		Record getRecord=new Record();
+		getRecord=sqlSession.selectOne("mapper.PlayerMapper.getUserRecord", map);
+		return getRecord;
+	}
+	
+	//종목별 경기장 정보 가져오기
+	public Grounds getUserGround(String user_Id, String sports) {
+		Map<String, String> map=new HashMap<>();
+		map.put("user_Id", user_Id);
+		map.put("sports", sports);
+		
+		Grounds getGrounds=new Grounds();
+		getGrounds=sqlSession.selectOne("mapper.PlayerMapper.getGrounds", map);
+		return getGrounds;
+	}
 }
