afaac2b3891e58986a79f681fa242a081a0b3306
jimin <afrocat@naver.com>
2016-10-20 18:09:32 +0900

    player commit 1020

--- MatchingService/src/action/PlayerAction.java
+++ MatchingService/src/action/PlayerAction.java
@@ -7,16 +7,18 @@ import org.apache.struts2.interceptor.SessionAware;
 import com.opensymphony.xwork2.ActionSupport;
 
 import dao.PlayerDAO;
+import vo.Grounds;
 import vo.Player;
-import vo.Team;
+import vo.Record;
 
 public class PlayerAction extends ActionSupport implements SessionAware {
 	Player player;
-	/*Team team;
-	Matching matching;*/
+	Record record;
+	Grounds grounds;
 	Map<String, Object> session;
 	PlayerDAO dao=new PlayerDAO();
 	
+	String sports;
 	String loginId;	//session 확인용
 	String message;	//에러 메세지 전송용
 	
@@ -27,6 +29,24 @@ public class PlayerAction extends ActionSupport implements SessionAware {
 	}
 	public void setPlayer(Player player) {
 		this.player = player;
+	}
+	public Record getRecord() {
+		return record;
+	}
+	public void setRecord(Record record) {
+		this.record = record;
+	}
+	public Grounds getGrounds() {
+		return grounds;
+	}
+	public void setGrounds(Grounds grounds) {
+		this.grounds = grounds;
+	}
+	public String getSports() {
+		return sports;
+	}
+	public void setSports(String sports) {
+		this.sports = sports;
 	}
 	public String getLoginId() {
 		return loginId;
@@ -40,29 +60,44 @@ public class PlayerAction extends ActionSupport implements SessionAware {
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	
 	//method
 	//개인정보 가져오기
 	public String getUserInfo() {
-		loginId=(String)session.get("loginId");
-		System.out.println(">> "+loginId+"로 정보를 가져옵니다.");
+	/*	player.setuser_Id((String)session.get("loginId"));
+		System.out.println(">> "+player.getuser_Id()+"로 개인정보 가져옵니다.");
+		player=dao.getUserInfo(player.getuser_Id());*/
+		player=dao.getUserInfo("TEST");
+		System.out.println(">> 정보 불러오기 완료!");
 		return SUCCESS;
 	}
 	
+	
 	//종목별 전적 가져오기
 	public String getUserRecord() {
+		/*player.setuser_Id((String)session.get("loginId"));
+		System.out.println(">> "+player.getuser_Id()+"로 종목별 전적 가져옵니다.");
+		record=dao.getUserRecord(player.getuser_Id());*/
+		record=dao.getUserRecord("TEST");
+		System.out.println(">> 정보 불러오기 완료!");
 		return SUCCESS;
 	}
 	
 	//종목별 경기장 정보 가져오기
 	public String getUserGround() {
+		/*player.setuser_Id((String)session.get("loginId"));
+		System.out.println(">> "+player.getuser_Id()+"로 종목별 경기장 정보 가져옵니다.");
+		grounds=dao.getUserGround(player.getuser_Id());*/
+		grounds=dao.getUserGround("TEST");
+		System.out.println(">> 정보 불러오기 완료!");
 		return SUCCESS;
 	}
+	
 	
 	//session
 	@Override
 	public void setSession(Map<String, Object> arg0) {
 		session=arg0;
 	}
-	
 }
--- MatchingService/src/dao/PlayerDAO.java
+++ MatchingService/src/dao/PlayerDAO.java
@@ -20,24 +20,16 @@ public class PlayerDAO {
 	}
 	
 	//종목별 전적 가져오기
-	public Record getUserRecord(String user_Id, String sports) {
-		Map<String, String> map=new HashMap<>();
-		map.put("user_Id", user_Id);
-		map.put("sports", sports);
-		
+	public Record getUserRecord(String user_Id) {
 		Record getRecord=new Record();
-		getRecord=sqlSession.selectOne("mapper.PlayerMapper.getUserRecord", map);
+		getRecord=sqlSession.selectOne("mapper.PlayerMapper.getUserRecord", user_Id);
 		return getRecord;
 	}
 	
 	//종목별 경기장 정보 가져오기
-	public Grounds getUserGround(String user_Id, String sports) {
-		Map<String, String> map=new HashMap<>();
-		map.put("user_Id", user_Id);
-		map.put("sports", sports);
-		
+	public Grounds getUserGround(String user_Id) {
 		Grounds getGrounds=new Grounds();
-		getGrounds=sqlSession.selectOne("mapper.PlayerMapper.getGrounds", map);
+		getGrounds=sqlSession.selectOne("mapper.PlayerMapper.getGrounds", user_Id);
 		return getGrounds;
 	}
 }
